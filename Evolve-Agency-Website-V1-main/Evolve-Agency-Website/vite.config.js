538b3e023f201686c048ece5af3d88a7834d5979
Mario9290 <136044636+Mario9290@users.noreply.github.com>
2025-09-02 21:15:50 -0600

    Change base path for Vite configuration

--- Evolve-Agency-Website-V1-main/Evolve-Agency-Website/vite.config.js
+++ Evolve-Agency-Website-V1-main/Evolve-Agency-Website/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: must match your repo name for project Pages
-  base: '/Evolve-Agency-Website-V1/',
+  base: '/Evolve-Website/',
   build: { outDir: 'dist' }
 })
